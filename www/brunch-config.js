bad0ba55ed9185aeb2bd9620506d40283e302a07
Zach Hobbs <zachhobbs+git@gmail.com>
2016-12-22 00:32:13 -0800

    Cleaning up

--- www/brunch-config.js
+++ www/brunch-config.js
@@ -1,11 +1,11 @@
 // See http://brunch.io for documentation.
-exports.files = {
-  javascripts: {
-	joinTo: {
-	    'app.js':'app/js/*.js'
-	}
+module.exports = {
+  files: {
+    javascripts: {joinTo: 'app.js'},
+    stylesheets: {joinTo: 'app.css'},
+    templates: {joinTo: 'app.js'}
   },
-  stylesheets: {
-	joinTo: 'app.css'
+  paths: {
+    watched: ['js','css','assets']
   }
-};
+}
